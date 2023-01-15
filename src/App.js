5366269596f4fb621aba64ddb8919aa405efbfaa
AK9789 <112373687+AK9789@users.noreply.github.com>
2023-01-15 13:41:46 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header/Header';
+import Doctors from './components/Doctor/Doctor';
+import Banner from './components/Banner/Banner';
+import RegisterDoctor from './components/RegisterDoctor/RegisterDoctor';
+import RegisterPatient from './components/RegisterPatient/RegisterPatient';
+import SpecificDoctor from './components/Doctor/SpecificDoctor';
+import Login from './components/Login/Login';
+import OneDoctor from './components/Doctor/OneDoctor';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+        <Routes>
+            
+            <Route path='/' element={<Banner/>}/>
+            <Route path='/login' element={<Login></Login>}></Route>
+            <Route path='/doctors' element={<Doctors/>}/>
+            <Route path='/registerDoctor' element={<RegisterDoctor/>}/>
+            <Route path='/registerPatient' element={<RegisterPatient/>}/>
+            <Route path='/onedoc/:docId'element={<SpecificDoctor></SpecificDoctor>}/>
+            <Route path='/oneDoctor' element={<OneDoctor></OneDoctor>}/>
+        </Routes>
     </div>
   );
 }
